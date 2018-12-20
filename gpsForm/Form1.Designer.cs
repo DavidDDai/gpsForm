018b4835a6958165442d611e297fd18aae0e2200
davidddai <daijiantao@163.com>
2018-12-20 13:32:13 +0800

    first upload

---
+++ gpsForm/Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace gpsForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lb_state = new System.Windows.Forms.Label();
+            this.lb_alt = new System.Windows.Forms.Label();
+            this.lb_lat = new System.Windows.Forms.Label();
+            this.lb_lon = new System.Windows.Forms.Label();
+            this.lb_num = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(118, 42);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(540, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "连接";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(59, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "端口：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(269, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "波特率：";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(361, 42);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 21);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(36, 79);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(457, 192);
+            this.textBox3.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(514, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "状态：";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(514, 135);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "卫星数：";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(514, 171);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 12);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "经度：";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(514, 204);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(41, 12);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "纬度：";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(514, 238);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(41, 12);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "海拔：";
+            // 
+            // lb_state
+            // 
+            this.lb_state.AutoSize = true;
+            this.lb_state.Location = new System.Drawing.Point(574, 101);
+            this.lb_state.Name = "lb_state";
+            this.lb_state.Size = new System.Drawing.Size(41, 12);
+            this.lb_state.TabIndex = 11;
+            this.lb_state.Text = "状态：";
+            // 
+            // lb_alt
+            // 
+            this.lb_alt.AutoSize = true;
+            this.lb_alt.Location = new System.Drawing.Point(574, 238);
+            this.lb_alt.Name = "lb_alt";
+            this.lb_alt.Size = new System.Drawing.Size(41, 12);
+            this.lb_alt.TabIndex = 12;
+            this.lb_alt.Text = "状态：";
+            // 
+            // lb_lat
+            // 
+            this.lb_lat.AutoSize = true;
+            this.lb_lat.Location = new System.Drawing.Point(574, 204);
+            this.lb_lat.Name = "lb_lat";
+            this.lb_lat.Size = new System.Drawing.Size(41, 12);
+            this.lb_lat.TabIndex = 13;
+            this.lb_lat.Text = "状态：";
+            // 
+            // lb_lon
+            // 
+            this.lb_lon.AutoSize = true;
+            this.lb_lon.Location = new System.Drawing.Point(574, 171);
+            this.lb_lon.Name = "lb_lon";
+            this.lb_lon.Size = new System.Drawing.Size(41, 12);
+            this.lb_lon.TabIndex = 14;
+            this.lb_lon.Text = "状态：";
+            // 
+            // lb_num
+            // 
+            this.lb_num.AutoSize = true;
+            this.lb_num.Location = new System.Drawing.Point(574, 135);
+            this.lb_num.Name = "lb_num";
+            this.lb_num.Size = new System.Drawing.Size(41, 12);
+            this.lb_num.TabIndex = 15;
+            this.lb_num.Text = "状态：";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(731, 450);
+            this.Controls.Add(this.lb_num);
+            this.Controls.Add(this.lb_lon);
+            this.Controls.Add(this.lb_lat);
+            this.Controls.Add(this.lb_alt);
+            this.Controls.Add(this.lb_state);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "MyGPS";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lb_state;
+        private System.Windows.Forms.Label lb_alt;
+        private System.Windows.Forms.Label lb_lat;
+        private System.Windows.Forms.Label lb_lon;
+        private System.Windows.Forms.Label lb_num;
+    }
+}
+
---
+++ gpsForm/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gpsForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string com_port = textBox1.Text;
+            string baud_str = textBox2.Text;
+            if (textBox1.Text == null || textBox1.Text.Trim() == "") return;
+            if (textBox2.Text == null || textBox2.Text.Trim() == "") return;
+            serialPort1.PortName = com_port;
+            serialPort1.BaudRate = int.Parse(baud_str);
+            serialPort1.Open();
+            
+            
+        }
+    }
+}
